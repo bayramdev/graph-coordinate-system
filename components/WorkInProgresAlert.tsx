0885aae7ad601aec647171f2c5fece5ef5e20511
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-12-20 21:20:37 +0000

    Create WorkInProgresAlert component

---
+++ components/WorkInProgresAlert.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Alert from "react-bootstrap/Alert";
+
+export const WorkInProgressAlert = () => {
+  return (
+    <Alert variant="info">
+      <Alert.Heading>This page is work in progress</Alert.Heading>
+      <p>There is not much going on here. This page is in development.</p>
+    </Alert>
+  );
+};
