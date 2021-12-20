a48a9faf8b2efb43550587503e1541ed07e26575
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-12-20 21:20:58 +0000

    Create HeaderedPage component

---
+++ components/HeaderedPage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Head from "next/head";
+import Container from "react-bootstrap/Container";
+import { Header } from "./Header";
+
+interface HeaderedPageProps {
+  title?: string | undefined;
+}
+
+export const HeaderedPage: React.FC<HeaderedPageProps> = (props) => {
+  const title = props.title ?? "Graph Coordinate System";
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <Container style={{ marginTop: 24 }}>{props.children}</Container>
+    </div>
+  );
+};
