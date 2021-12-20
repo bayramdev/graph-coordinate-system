fa8def11e3374a593e43ed812508a843d3f3fb08
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-12-20 21:40:51 +0000

    Fix typo in WorkInProgresAlert.tsx filename

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import { HeaderedPage } from "@/components/HeaderedPage";
-import { WorkInProgressAlert } from "@/components/WorkInProgresAlert";
+import { WorkInProgressAlert } from "@/components/WorkInProgressAlert";
 
 const Home: NextPage = () => {
   return (
