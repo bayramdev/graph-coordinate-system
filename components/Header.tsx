34a6fb473f5630146a41ed2b994b6417495ec702
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-12-20 20:40:16 +0000

    Create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+export const Header = () => {
+  return (
+    <header>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Graph Coordinate System</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Link href="/extract">
+                <Nav.Link>Extract</Nav.Link>
+              </Link>
+
+              <Link href="/coordinate">
+                <Nav.Link>Coordinates</Nav.Link>
+              </Link>
+
+              <Link href="/graph" passHref>
+                <Nav.Link>Graph</Nav.Link>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Button } from "react-bootstrap";
+import { Header } from "@/components/Header";
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Button variant="primary">hello world</Button>
+      <Header />
     </div>
   );
 };
