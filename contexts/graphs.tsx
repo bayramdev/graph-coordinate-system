6fca5f5c2bf762e2ec13d3ed1678176fff39e1f0
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-12-23 02:02:26 +0300

    Add current graph data to graphs context

--- contexts/graphs.tsx
+++ contexts/graphs.tsx
@@ -1,22 +1,33 @@
 import { createContext, useState, Dispatch, SetStateAction } from "react";
-import { GraphsType } from "@/types/graphs";
+import { GraphsType, GraphType } from "@/types/graphs";
 
-type GraphsContextType = {
-  graphs: GraphsType;
-  setGraphs: Dispatch<SetStateAction<GraphsType>>;
+export type GraphsContextType = {
+  graphs: GraphsType | null;
+  setGraphs: Dispatch<SetStateAction<GraphsType | null>>;
+  current: GraphType | null;
+  changeCurrent: (label: string) => void;
 };
 
 const defaultContext = {} as GraphsContextType;
 export const GraphsContext = createContext<GraphsContextType>(defaultContext);
 
 export const GraphsProvider: React.FC = ({ children }) => {
-  const [graphs, setGraphs] = useState<GraphsType>(null);
+  const [graphs, setGraphs] = useState<GraphsType | null>(null);
+  const [current, setCurrent] = useState<GraphType | null>(null);
+
+  const changeCurrent = (label: string) => {
+    if (!graphs?.graphs) return;
+    const graph = graphs?.graphs.filter((g) => g.label === label)[0];
+    setCurrent(graph);
+  };
 
   return (
     <GraphsContext.Provider
       value={{
         graphs,
         setGraphs,
+        current,
+        changeCurrent,
       }}
     >
       {children}
--- types/graphs.ts
+++ types/graphs.ts
@@ -1,10 +1,12 @@
 type NodeID = string;
 type EdgeMetadata = { factor: number };
 
+export type GraphType = {
+  label: string;
+  nodes: { id: NodeID; label: string }[];
+  edges: { source: NodeID; target: NodeID; metadata: EdgeMetadata }[];
+};
+
 export type GraphsType = {
-  graphs: {
-    label: string;
-    nodes: { id: NodeID; label: string }[];
-    edges: { source: NodeID; target: NodeID; metadata: EdgeMetadata }[];
-  }[];
-} | null;
+  graphs: GraphType[];
+};
