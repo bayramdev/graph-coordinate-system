3b023394d2d8a36b9e9561f4a5349147620308f4
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-12-23 00:20:35 +0300

    Implement reading JSON graphs data

--- components/HeaderDataButton.tsx
+++ components/HeaderDataButton.tsx
@@ -2,26 +2,32 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { GraphsContext } from "@/contexts/graphs";
+import { useJsonGraphs } from "hooks/useJsonGraph";
 
 const HeaderDataButton = () => {
   const { graphs, setGraphs } = React.useContext(GraphsContext);
+  const { loading, setJsonGraphs } = useJsonGraphs();
 
-  const handleFile: React.ChangeEventHandler = (e) => {
-    e.preventDefault();
-    setGraphs({});
+  const handleFile: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (!e.target.files?.length) return;
+    setJsonGraphs(e.target.files[0]);
   };
 
   const handleReset: React.MouseEventHandler = (e) => {
     setGraphs(null);
   };
 
-  return graphs ? (
+  return loading ? (
+    <Button variant="warning" disabled={true}>
+      Loading...
+    </Button>
+  ) : graphs ? (
     <Button variant="warning" onClick={handleReset}>
       Reset data
     </Button>
   ) : (
     <Form.Group controlId="formFile">
-      <Form.Control type="file" accept=".xls,.xlsx" onChange={handleFile} />
+      <Form.Control type="file" accept=".json" onChange={handleFile} />
     </Form.Group>
   );
 };
---
+++ hooks/useJsonGraph.ts
@@ -0,0 +1,28 @@
+import React from "react";
+import { GraphsContext } from "@/contexts/graphs";
+import { GraphsType } from "@/types/graphs";
+
+const readJson = async (file: File): Promise<Object> => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.onload = (event) =>
+      resolve(JSON.parse(String(event.target?.result || null)));
+    fileReader.onerror = (error) => reject(error);
+    fileReader.readAsText(file);
+  });
+};
+
+export const useJsonGraphs = () => {
+  const { setGraphs } = React.useContext(GraphsContext);
+  const [loading, setLoading] = React.useState(false);
+
+  return {
+    loading,
+    setJsonGraphs: async (file: File) => {
+      setLoading(true);
+      const json = await readJson(file);
+      setGraphs(json as GraphsType);
+      setLoading(false);
+    },
+  };
+};
