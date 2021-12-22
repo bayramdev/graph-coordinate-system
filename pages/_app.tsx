24a8cbd47b8956e0ccc8284a979978b5b254ede9
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-12-23 00:20:48 +0300

    Change bootswatch theme to lumen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { GraphsProvider } from "@/contexts/graphs";
-import "bootswatch/dist/darkly/bootstrap.min.css";
+import "bootswatch/dist/lumen/bootstrap.min.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
