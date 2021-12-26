3c117ab2bd611de9d30dc20d3a0eedbac30962c2
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-12-27 01:56:26 +0300

    Make size of Graph3DCanvas responsive

--- components/Graph3DCanvas.tsx
+++ components/Graph3DCanvas.tsx
@@ -8,23 +8,25 @@ type Graph3DCanvasProps = {
 
 const Graph3DCanvas: React.FC<Graph3DCanvasProps> = (props) => {
   return (
-    <ForceGraph3D
-      graphData={{
-        nodes: props.context.current.nodes,
-        links: props.context.current.edges.map((edge) => ({
-          source: edge.source,
-          target: edge.target,
-          label: edge.metadata.factor,
-        })),
-      }}
-      nodeLabel="label"
-      linkLabel="label"
-      linkDirectionalArrowLength={3.5}
-      linkDirectionalArrowRelPos={1}
-      linkCurvature={0.25}
-      width={1000}
-      height={600}
-    />
+    <div className="bg-secondary vh-100 w-100 overflow-hidden d-flex justify-content-center">
+      <ForceGraph3D
+        graphData={{
+          nodes: props.context.current.nodes,
+          links: props.context.current.edges.map((edge) => ({
+            source: edge.source,
+            target: edge.target,
+            label: edge.metadata.factor,
+          })),
+        }}
+        nodeLabel="label"
+        linkLabel="label"
+        linkDirectionalArrowLength={3.5}
+        linkDirectionalArrowRelPos={1}
+        linkCurvature={0.25}
+        width={1280}
+        height={720}
+      />
+    </div>
   );
 };
 
