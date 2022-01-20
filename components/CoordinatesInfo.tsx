ff431d13592e70e823f80f6cfa1b610c6a33c8e5
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2022-01-20 03:57:08 +0300

    Refactor coordinates info calculations

--- components/CoordinatesInfo.tsx
+++ components/CoordinatesInfo.tsx
@@ -8,9 +8,37 @@ type CoordinatesInfoProps = {
 };
 
 const CoordinatesInfo: React.FC<CoordinatesInfoProps> = (props) => {
+  const selected = props.context.current.nodes[props.selectedIndex];
+  const directTargetIndexesOfSelected = props.context.matrix
+    .map((row, index) => (row[props.selectedIndex] !== 0 ? index : null))
+    .filter((nullable) => nullable) as number[];
+  const indirectTargetIndexesOfSelected = directTargetIndexesOfSelected.flatMap(
+    (targetIndex) =>
+      props.context.matrix
+        .map((row, index) =>
+          row[targetIndex] !== 0 ? [targetIndex, index] : null
+        )
+        .filter((nullable) => nullable) as number[][]
+  );
+  const directSourceIndexesOfSelected = props.context.matrix[
+    props.selectedIndex
+  ]
+    .map((value, colIndex) => (value ? colIndex : null))
+    .filter((nullable) => nullable) as number[];
+  const indirectSourceIndexesOfSelected = directSourceIndexesOfSelected.flatMap(
+    (sourceIndex) =>
+      props.context.matrix[sourceIndex]
+        .map((value, index) => (value ? [index, sourceIndex] : null))
+        .filter((nullable) => nullable) as number[][]
+  );
+  
   return (
     <div className="mt-4">
-      <p>Displaying analyze for the selected node labeled <strong>{props.context.current.nodes[props.selectedIndex].label}</strong>...</p>
+      <p>
+        Displaying analyze for the selected node labeled{" "}
+        <strong>{selected.label}</strong>
+        ...
+      </p>
 
       <h2 className="mt-4 mb-2">Level 1 Targeted Nodes</h2>
       <Table striped bordered hover>
@@ -22,15 +50,13 @@ const CoordinatesInfo: React.FC<CoordinatesInfoProps> = (props) => {
           </tr>
         </thead>
         <tbody>
-          {props.context.matrix.map((row, rowIndex) =>
-            row[props.selectedIndex] ? (
-              <tr>
-                <td>{rowIndex}</td>
-                <td>{row[props.selectedIndex]}</td>
-                <td>{props.context.current.nodes[rowIndex].label}</td>
-              </tr>
-            ) : null
-          )}
+          {directTargetIndexesOfSelected.map((target) => (
+            <tr>
+              <td>{target}</td>
+              <td>{props.context.matrix[target][props.selectedIndex]}</td>
+              <td>{props.context.current.nodes[target].label}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
 
@@ -45,22 +71,14 @@ const CoordinatesInfo: React.FC<CoordinatesInfoProps> = (props) => {
           </tr>
         </thead>
         <tbody>
-          {props.context.matrix.map((row, rowIndex) =>
-            row[props.selectedIndex]
-              ? props.context.matrix.map((rowInner, rowInnerIndex) =>
-                  rowInner[rowIndex] ? (
-                    <tr>
-                      <td>{rowIndex}</td>
-                      <td>{rowInner[rowIndex]}</td>
-                      <td>{props.context.current.nodes[rowIndex].label}</td>
-                      <td>
-                        {props.context.current.nodes[rowInnerIndex].label}
-                      </td>
-                    </tr>
-                  ) : null
-                )
-              : null
-          )}
+          {indirectTargetIndexesOfSelected.map(([source, target]) => (
+            <tr>
+              <td>{target}</td>
+              <td>{props.context.matrix[target][source]}</td>
+              <td>{props.context.current.nodes[source].label}</td>
+              <td>{props.context.current.nodes[target].label}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
 
@@ -74,15 +92,13 @@ const CoordinatesInfo: React.FC<CoordinatesInfoProps> = (props) => {
           </tr>
         </thead>
         <tbody>
-          {props.context.matrix[props.selectedIndex].map((value, colIndex) =>
-            value ? (
-              <tr>
-                <td>{colIndex}</td>
-                <td>{value}</td>
-                <td>{props.context.current.nodes[colIndex].label}</td>
-              </tr>
-            ) : null
-          )}
+          {directSourceIndexesOfSelected.map((source) => (
+            <tr>
+              <td>{source}</td>
+              <td>{props.context.matrix[props.selectedIndex][source]}</td>
+              <td>{props.context.current.nodes[source].label}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
 
@@ -97,20 +113,14 @@ const CoordinatesInfo: React.FC<CoordinatesInfoProps> = (props) => {
           </tr>
         </thead>
         <tbody>
-          {props.context.matrix[props.selectedIndex].map((value, colIndex) =>
-            value
-              ? props.context.matrix[colIndex].map((valueInner, indexInner) =>
-                  valueInner ? (
-                    <tr>
-                      <td>{colIndex}</td>
-                      <td>{valueInner}</td>
-                      <td>{props.context.current.nodes[indexInner].label}</td>
-                      <td>{props.context.current.nodes[colIndex].label}</td>
-                    </tr>
-                  ) : null
-                )
-              : null
-          )}
+          {indirectSourceIndexesOfSelected.map(([source, target]) => (
+            <tr>
+              <td>{target}</td>
+              <td>{props.context.matrix[target][source]}</td>
+              <td>{props.context.current.nodes[source].label}</td>
+              <td>{props.context.current.nodes[target].label}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
