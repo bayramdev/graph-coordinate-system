abcc1e4395433249f0f492e67d78edbf5596f228
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2022-01-20 05:42:34 +0300

    Darken node and edge colors

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,5 +1,5 @@
 export const colorNodeBy = (certain: boolean | null) =>
-  certain === null ? "#888888" : certain ? "#22dd22" : "#dd2222";
+  certain === null ? "#888888" : certain ? "#22cc22" : "#cc2222";
 
 export const colorEdgeBy = (certain: boolean | null) =>
   colorNodeBy(certain) + "80"; // alpha channel
